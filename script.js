1fb0fe73b388b3659ec226064518384b08f29615
Kamil M <kamilmaz9206@gmail.com>
2020-01-13 17:49:53 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// var firstOverlap = document.querySelector('.aboutMe');
+
+
+// function move() {
+//     firstOverlap.classList.location('about');
+// }
+
+// firstOverlap.addEventListener('click', move);
